dc6523d2e27796e4dce57caa05692a03026968c2
Jack Yeh <jack.yeh@gmail.com>
2015-07-23 22:58:09 -0600

    add functionality: delete Authorization header before vcr record - this prevents tokens from being saved into vcr cassettes; this is an added security measure

--- test/test_helper.rb
+++ test/test_helper.rb
@@ -9,6 +9,10 @@ class ActiveSupport::TestCase
   VCR.configure do |config|
     config.cassette_library_dir = "fixtures/vcr_cassettes"
     config.hook_into :webmock # or :fakeweb
+    config.default_cassette_options = { :serialize_with => :json }
+    config.before_record do |r|
+      r.request.headers.delete("Authorization")
+    end
   end
 
   # Setup all fixtures in test/fixtures/*.yml for all tests in alphabetical order.
